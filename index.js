aa82415563ed4785d2fe8065d5969f3e0e485570
BeastieNate5 <nhagan6@att.net>
2024-09-07 20:17:28 -0400

    Improved Menu Tabs and JS refactor

--- index.js
+++ index.js
@@ -1,35 +1,80 @@
 var selecables = document.getElementsByClassName("selectable");
 
-for (i = 0; i < selecables.length; i++) {
-    selecables[i].addEventListener("mouseover", (event) => {
-        var ele = event.target;
 
+function getDarkSlider(selectable_p) {
+    var children = selectable_p.children;
+    
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].classList.contains("dark-slider")) {
+            return children[i];
+        }
+    }
+
+    return null;
+}
+
+function getSelectableIcon(selectable_p) {
+    var children = selectable_p.children;
+
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].classList.contains("selectable-icon")) {
+            return children[i];
+        }
+    }
+
+    return null;
+}
+
+function toggleSelectableIcon(selectable_p) {
+    var icon = getSelectableIcon(selectable_p);
+    var icon_src = icon.getAttribute("src");
+
+    if (icon_src == "/assets/images/icon.svg") {
+        icon.setAttribute("src", "/assets/images/icon_active.svg");
+    } else {
+        icon.setAttribute("src", "/assets/images/icon.svg");
+    }
+}
+
+for (i = 0; i < selecables.length; i++) {
+    var darkslider = document.createElement("div");
+    darkslider.setAttribute("class", "dark-slider");
+    selecables[i].appendChild(darkslider);
+}
+
+
+for (let i = 0; i < selecables.length; i++) {
+    selecables[i].addEventListener("mouseenter", (event) => {
+        var ele = event.target;
+        
         if (!ele.classList.contains("selectable")) {
             ele = ele.parentElement;
         }
 
         ele.classList.add("selectable-active");
         
-        var menuIcon = ele.firstElementChild;
-        var darkslider = ele.lastElementChild;
+        var menuIcon = getSelectableIcon(ele);
+        var darkslider = getDarkSlider(ele);
 
+        toggleSelectableIcon(ele);
         darkslider.classList.add("dark-slider-on");
-        menuIcon.classList.add("menu-icon-active");
+        menuIcon.classList.add("selectable-icon-active");
     });
 
     selecables[i].addEventListener("mouseleave", (event) => {
         var ele = event.target;
-
+        console.log("Off");
         if (!ele.classList.contains("selectable")) {
             ele = ele.parentElement;
         }
         
         ele.classList.remove("selectable-active");
 
-        var menuIcon = ele.firstElementChild;
-        var darkslider = ele.lastElementChild;
+        var menuIcon = getSelectableIcon(ele);
+        var darkslider = getDarkSlider(ele);
 
+        toggleSelectableIcon(ele);
         darkslider.classList.remove("dark-slider-on"); 
-        menuIcon.classList.remove("menu-icon-active");
+        menuIcon.classList.remove("selectable-icon-active");
     });
 }
